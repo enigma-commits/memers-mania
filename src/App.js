13dcd494d1ce18fd6bd2e6a277d3153ac395c8a3
akhilesh singh <akhileshsingh2314@gmail.com>
2022-02-07 21:28:15 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
-import Home from "./Screens/Home.js"
+import Home from "./Screens/Home";
+import Dashboard from "./Screens/Dashboard";
 export default function App() {
-  return (
-    <Home/>
-  );
+  return <Dashboard />;
 }
