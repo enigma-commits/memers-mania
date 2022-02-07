68913fec030a436bb9201b08915cca1173f85573
Ratnesh <rktssgk@gmail.com>
2022-02-07 22:37:50 +0530

    *

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 
 import Home from "./Screens/Home.js";
 import CreatePost from "./Screens/CreatePost.js";
+import Dashboard from "./Screens/Dashboard.js";
 
 import { Box } from "@mui/system";
 import ResponsiveAppBar from "./Components/Navbar/ResponsiveAppBar";
 export default function App() {
-
-  return <Dashboard />;
-  return <Home />;
+	return (
+		<Box
+			sx={{
+				backgroundColor: "#1c1f20",
+				minHeight: "100vh",
+			}}>
+			<Box component='header' sx={{ position: "sticky", top: 0, zIndex: 50 }}>
+				<ResponsiveAppBar />
+			</Box>
+			<Dashboard />
+		</Box>
+	);
 }
