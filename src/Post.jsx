ce93233bc34e111a97aecf9d0a409020e92a3837
akhilesh singh <akhileshsingh2314@gmail.com>
2022-02-07 20:15:56 +0530

    added tooltip

--- src/Post.jsx
+++ src/Post.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/system";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, IconButton } from "@mui/material";
+import { CardActionArea, IconButton, Tooltip } from "@mui/material";
 import ArrowDownwardOutlinedIcon from "@mui/icons-material/ArrowDownwardOutlined";
 import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
 import ModeCommentOutlinedIcon from "@mui/icons-material/ModeCommentOutlined";
@@ -44,17 +44,23 @@ export default function Post() {
         }}
       >
         <Box>
-          <IconButton>
-            <ArrowUpwardOutlinedIcon fontSize="large" />
-          </IconButton>
-          <IconButton>
-            <ArrowDownwardOutlinedIcon fontSize="large" />
-          </IconButton>
+          <Tooltip title="upvote">
+            <IconButton>
+              <ArrowUpwardOutlinedIcon fontSize="large" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="downvote">
+            <IconButton>
+              <ArrowDownwardOutlinedIcon fontSize="large" />
+            </IconButton>
+          </Tooltip>
         </Box>
         <Box>
-          <IconButton>
-            <ModeCommentOutlinedIcon fontSize="large" />
-          </IconButton>
+          <Tooltip title="comment">
+            <IconButton>
+              <ModeCommentOutlinedIcon fontSize="large" />
+            </IconButton>
+          </Tooltip>
         </Box>
       </CardActions>
     </Card>
