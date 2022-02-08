07bdda17794b39343a7b7214dcc15c814de298a2
Ratnesh <rktssgk@gmail.com>
2022-02-08 21:18:18 +0530

    created models and routes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,23 @@ import express from "express";
 import connectDB from "./config/connectDB.js";
 import dotenv from "dotenv";
 import colors from "colors";
+
+import "./models/userModel.js";
+import "./models/postModel.js";
+
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => {
 	res.send("Api is running......");
 });
