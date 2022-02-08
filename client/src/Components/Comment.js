6b39130c9c69e381f38b491dc1879844fb6d9fcf
akhilesh singh <akhileshsingh2314@gmail.com>
2022-02-08 21:36:19 +0530

    added comment section

--- client/src/Components/Comment.js
+++ client/src/Components/Comment.js
@@ -1,5 +1,49 @@
 import React from "react";
 import { Box } from "@mui/system";
-export const Comment = () => {
-  return <Box component="div"></Box>;
+import Typography from "@mui/material/Typography";
+import { Avatar } from "@mui/material";
+export const Comment = (props) => {
+  return (
+    <Box component="div">
+      <Box
+        sx={{
+          color: "white",
+          display: "flex",
+          minHeight: "10vh",
+          pt: "2%",
+          pl: "2%",
+          mt: "2%",
+          ml: "10%",
+          mr: "10%",
+          bgcolor: "#1a1a1b",
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              mr: "10px",
+              ml: "15px",
+              pb: "15px",
+            }}
+          >
+            <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+              <Box sx={{ pr: "10px" }}>
+                <Avatar sx={{ bgcolor: "deepOrange[500]" }}>U</Avatar>
+              </Box>
+              <Typography variant="h5">User</Typography>
+            </Box>
+            <Box
+              sx={{ mt: "15px", pl: "15px", display: "flex", flexWrap: "wrap" }}
+            >
+              <Typography variant="p">
+                Hello this is my comment
+                ajsdalfksdjflkasdjf;lksdjf;alksdjf;askldfjasd;lkfjs;dlkfajsd;fkljasd;flkrwerqwerqwer
+                afkjsd;flkajsd;fkjadf;lksdjaf;klsdjf
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
--- client/src/Screens/SinglePostScreen.js
+++ client/src/Screens/SinglePostScreen.js
@@ -1,11 +1,16 @@
 import { Box } from "@mui/system";
 import React from "react";
 import SinglePost from "../Components/SinglePost";
+import { Comment } from "../Components/Comment";
 
 export const SinglePostScreen = () => {
   return (
     <Box component="div">
       <SinglePost image={"./tempimages/1116286.jpg"} />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
     </Box>
   );
 };
